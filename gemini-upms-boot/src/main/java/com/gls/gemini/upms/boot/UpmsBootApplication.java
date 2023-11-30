cc5c044cd0c9f0ddd345cf69486af78fdfab2eb3
李智勇 <george510257@gmail.com>
2023-11-30 17:52:58 +0800

    init

---
+++ gemini-upms-boot/src/main/java/com/gls/gemini/upms/boot/UpmsBootApplication.java
@@ -0,0 +1,12 @@
+package com.gls.gemini.upms.boot;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpmsBootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UpmsBootApplication.class, args);
+    }
+}
