3f593f2f0aab5065cc3d2e7dbc0b5434f5ba73d4
李智勇 <george510257@gmail.com>
2023-12-01 14:28:53 +0800

    添加基础配置

--- gemini-upms-boot/src/main/java/com/gls/gemini/upms/boot/UpmsBootApplication.java
+++ gemini-upms-boot/src/main/java/com/gls/gemini/upms/boot/UpmsBootApplication.java
@@ -4,8 +4,16 @@ package com.gls.gemini.upms.boot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * upms启动类
+ */
 @SpringBootApplication
 public class UpmsBootApplication {
+    /**
+     * upms启动入口
+     *
+     * @param args 启动参数
+     */
     public static void main(String[] args) {
         SpringApplication.run(UpmsBootApplication.class, args);
     }
