3a8fd072428af72acb157e22f0477a4220b31444
李智勇 <george510257@gmail.com>
2024-02-09 00:03:29 +0800

    添加xxl job 处理

--- gemini-upms-boot/src/main/java/com/gls/gemini/upms/boot/job/DemoJob.java
+++ gemini-upms-boot/src/main/java/com/gls/gemini/upms/boot/job/DemoJob.java
@@ -1,5 +1,7 @@
 package com.gls.gemini.upms.boot.job;
 
+import com.gls.gemini.upms.sdk.vo.DemoVo;
+import com.xxl.job.core.context.XxlJobHelper;
 import com.xxl.job.core.handler.annotation.XxlJob;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -10,6 +12,18 @@ public class DemoJob {
 
     @XxlJob("demoJob")
     public void demoJob() {
+        XxlJobHelper.log("demoJob start");
         log.info("demoJob");
+        XxlJobHelper.log("demoJob end");
+    }
+
+    @XxlJob("demoVo")
+    public DemoVo demoVo(DemoVo demoVo) {
+        XxlJobHelper.log("demoVo start");
+        XxlJobHelper.getJobParam();
+        XxlJobHelper.log("demoVo param: {}", demoVo);
+        log.info("demoVo: {}", demoVo);
+        XxlJobHelper.log("demoVo end");
+        return demoVo;
     }
 }
