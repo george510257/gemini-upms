b178ce2c1f3ab0de2cc0d0f5a02ec93eddcce968
李智勇 <george510257@gmail.com>
2024-02-14 23:14:18 +0800

    add :: 优化依赖

--- gemini-upms-boot/src/main/java/com/gls/gemini/upms/boot/web/service/DemoService.java
+++ gemini-upms-boot/src/main/java/com/gls/gemini/upms/boot/web/service/DemoService.java
@@ -6,22 +6,9 @@ import com.gls.gemini.upms.boot.web.entity.DemoEntity;
 import com.gls.gemini.upms.boot.web.mapper.DemoMapper;
 import com.gls.gemini.upms.sdk.vo.DemoVo;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Service
 public class DemoService extends BaseServiceImpl<DemoConverter, DemoMapper, DemoVo, DemoEntity> {
 
 
-    /**
-     * 批量保存
-     *
-     * @param demoVos demoVos
-     */
-    @Transactional(rollbackFor = Exception.class)
-    public void saveOrUpdateBatch(List<DemoVo> demoVos) {
-        List<DemoEntity> demoEntities = converter.convertList(demoVos);
-        this.saveOrUpdateBatch(demoEntities);
-    }
 }
