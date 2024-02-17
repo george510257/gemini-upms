fa0f8235a640150c3c2fc2aa050f44261287ef2a
李智勇 <george510257@gmail.com>
2024-02-17 16:21:28 +0800

    添加动态数据源

--- gemini-upms-boot/src/main/java/com/gls/gemini/upms/boot/web/entity/DemoEntity.java
+++ gemini-upms-boot/src/main/java/com/gls/gemini/upms/boot/web/entity/DemoEntity.java
@@ -22,22 +22,18 @@ public class DemoEntity extends BaseEntity {
     public static final String COL_NAME = "name";
     public static final String COL_DESCRIPTION = "description";
     public static final String COL_STATUS = "status";
-
     @Serial
     private static final long serialVersionUID = 1L;
-
     /**
      * 名称
      */
     private String name;
-
     /**
      * 描述
      */
     private String description;
-
     /**
      * 状态 0:正常 1:禁用
      */
-    private Integer status;
+    private Boolean status;
 }
--- gemini-upms-sdk/src/main/java/com/gls/gemini/upms/sdk/vo/DemoVo.java
+++ gemini-upms-sdk/src/main/java/com/gls/gemini/upms/sdk/vo/DemoVo.java
@@ -2,7 +2,6 @@ package com.gls.gemini.upms.sdk.vo;
 
 import com.alibaba.excel.annotation.ExcelProperty;
 import com.gls.gemini.sdk.core.vo.BaseVo;
-import com.gls.gemini.starter.excel.converter.StatusConverter;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -19,15 +18,22 @@ import lombok.EqualsAndHashCode;
 @Schema(title = "demo", description = "demo数据Vo")
 public class DemoVo extends BaseVo {
 
+    /**
+     * 名称
+     */
     @ExcelProperty(value = "名称")
     @Schema(title = "名称", description = "名称")
     private String name;
-
+    /**
+     * 描述
+     */
     @ExcelProperty(value = "描述")
     @Schema(title = "描述", description = "描述")
     private String description;
-
-    @ExcelProperty(value = "状态", converter = StatusConverter.class)
-    @Schema(title = "状态", description = "状态 0:正常 1:禁用")
-    private Integer status;
+    /**
+     * 状态 0:正常 1:禁用
+     */
+    @ExcelProperty(value = "状态 0:正常 1:禁用")
+    @Schema(title = "状态 0:正常 1:禁用", description = "状态 0:正常 1:禁用")
+    private Boolean status;
 }
