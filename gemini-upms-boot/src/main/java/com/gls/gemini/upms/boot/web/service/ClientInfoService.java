e5d8326a568fbf2aadf2df86dca2d99a4895c16f
李智勇 <lizy19@xiaopeng.com>
2024-05-07 14:27:17 +0800

    修改cache配置

--- gemini-upms-boot/src/main/java/com/gls/gemini/upms/boot/web/service/ClientInfoService.java
+++ gemini-upms-boot/src/main/java/com/gls/gemini/upms/boot/web/service/ClientInfoService.java
@@ -1,12 +1,14 @@
 package com.gls.gemini.upms.boot.web.service;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.gls.gemini.starter.data.redis.cache.CacheExpire;
 import com.gls.gemini.starter.mybatis.base.BaseServiceImpl;
 import com.gls.gemini.upms.boot.web.converter.ClientInfoConverter;
 import com.gls.gemini.upms.boot.web.entity.ClientInfoEntity;
 import com.gls.gemini.upms.boot.web.mapper.ClientInfoMapper;
 import com.gls.gemini.upms.sdk.vo.ClientInfoVo;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 /**
@@ -24,6 +26,8 @@ public class ClientInfoService extends BaseServiceImpl<ClientInfoConverter, Clie
      * @param clientId 客户端id
      * @return 客户端信息
      */
+    @CacheExpire(timeToLive = 300)
+    @Cacheable(value = "client-info-vo", key = "#clientId")
     public ClientInfoVo getByClientId(String clientId) {
         ClientInfoEntity clientInfoEntity = baseMapper.selectOne(new QueryWrapper<ClientInfoEntity>()
                 .eq(ClientInfoEntity.COL_CLIENT_ID, clientId));
